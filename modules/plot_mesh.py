3acc94acf85d263ca235840fc9ad86f805866eb3
dbarcene <david.barcene@ut.ac.pa>
2025-10-15 17:09:26 -0500

    created 2d example, updated plot-mesh.py with annotations

--- modules/plot_mesh.py
+++ modules/plot_mesh.py
@@ -3,7 +3,7 @@
 # File              : plot_mesh.py
 # Author            : David Barcene <david.barcene@utp.ac.pa>
 # Date              : 10.06.2022
-# Last Modified Date: 14.10.2025
+# Last Modified Date: 15.10.2025
 # Last Modified By  : David Barcene <david.barcene@utp.ac.pa>
 
 import numpy as np
@@ -28,6 +28,8 @@ choose = int(input("Input the number for the element_type you want to select:\n"
                    "1. 2DQU4N\n"
                    "2. 2DTR3N\n"
                    "elemnt_type: "))
+
+element_type = '2DTR3N'
 if choose == 1:
     element_type = '2DQU4N'
 elif choose == 2:
@@ -43,6 +45,17 @@ plt.figure(figsize=(8, 8))
 
 if element_type == '2DQU4N':
     element_counter = 1
+    for j in range(NoE):
+        plt.annotate(element_counter, xy=((NL[EL[j, 0]-1, 0] +
+                                           NL[EL[j, 1]-1, 0] +
+                                           NL[EL[j, 2]-1, 0] +
+                                           NL[EL[j, 3]-1, 0])/4,
+                                          (NL[EL[j, 0]-1, 1] +
+                                           NL[EL[j, 1]-1, 1] +
+                                           NL[EL[j, 2]-1, 1] +
+                                           NL[EL[j, 3]-1, 1])/4),
+                     color='blue')
+        element_counter += 1
 
     x0 = NL[EL[:, 0]-1, 0]
     y0 = NL[EL[:, 0]-1, 1]
@@ -53,13 +66,23 @@ if element_type == '2DQU4N':
     x3 = NL[EL[:, 3]-1, 0]
     y3 = NL[EL[:, 3]-1, 1]
 
-    plt.plot(np.array([x0, x1]), np.array([y0, y1]), 'red', linewidth=0.5)
-    plt.plot(np.array([x1, x2]), np.array([y1, y2]), 'red', linewidth=0.5)
-    plt.plot(np.array([x2, x3]), np.array([y2, y3]), 'red', linewidth=0.5)
-    plt.plot(np.array([x3, x0]), np.array([y3, y0]), 'red', linewidth=0.5)
+    plt.plot(np.array([x0, x1]), np.array([y0, y1]), 'red', linewidth=1)
+    plt.plot(np.array([x1, x2]), np.array([y1, y2]), 'red', linewidth=1)
+    plt.plot(np.array([x2, x3]), np.array([y2, y3]), 'red', linewidth=1)
+    plt.plot(np.array([x3, x0]), np.array([y3, y0]), 'red', linewidth=1)
 
 
 elif element_type == '2DTR3N':
+    element_counter = 1
+    for j in range(NoE):
+        plt.annotate(element_counter, xy=((NL[EL[j, 0]-1, 0] +
+                                           NL[EL[j, 1]-1, 0] +
+                                           NL[EL[j, 2]-1, 0])/3,
+                                          (NL[EL[j, 0]-1, 1] +
+                                           NL[EL[j, 1]-1, 1] +
+                                           NL[EL[j, 2]-1, 1])/3),
+                     color='blue')
+        element_counter += 1
 
     x0 = NL[EL[:, 0]-1, 0]
     y0 = NL[EL[:, 0]-1, 1]
@@ -68,8 +91,8 @@ elif element_type == '2DTR3N':
     x2 = NL[EL[:, 2]-1, 0]
     y2 = NL[EL[:, 2]-1, 1]
 
-    plt.plot(np.array([x0, x1]), np.array([y0, y1]), 'red', linewidth=0.5)
-    plt.plot(np.array([x1, x2]), np.array([y1, y2]), 'red', linewidth=0.5)
-    plt.plot(np.array([x2, x0]), np.array([y2, y0]), 'red', linewidth=0.5)
+    plt.plot(np.array([x0, x1]), np.array([y0, y1]), 'red', linewidth=1)
+    plt.plot(np.array([x1, x2]), np.array([y1, y2]), 'red', linewidth=1)
+    plt.plot(np.array([x2, x0]), np.array([y2, y0]), 'red', linewidth=1)
 
 plt.show()
